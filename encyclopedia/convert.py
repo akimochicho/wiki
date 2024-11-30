c8140e5c1fd28f1ebfe0ca9dc87ff99387de9858
akimochicho <124653915+akimochicho@users.noreply.github.com>
2024-11-30 23:40:18 +0800

    Add files via upload
    uploaded wiki project files after editing html's of entry and index pages

---
+++ encyclopedia/convert.py
@@ -0,0 +1,8 @@
+# import markdown
+
+# with open('entries/CSS.md', 'r') as f:
+#     text = f.read()
+#     html = markdown.markdown(text)
+
+# with open('entires/CSS.html', 'w') as f:
+#     f.write(html)
---
+++ encyclopedia/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from . import util
+
+from flask import Flask, render_template 
+ 
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+def title(request, name):
+    return render(request, "encyclopedia/entry_page.html",{
+        "title": util.title_change(name),
+        "name": util.get_entry(name), 
+    })
+
